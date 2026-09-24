fed7e6f5edf55b910c916b70425a86700bdd8d6a
Fabri519 <fabrizzio.lopezroldan@alu.frt.utn.edu.ar>
2026-09-23 23:04:32 -0300

    casi listo

--- js/specialtyCreate.js
+++ js/specialtyCreate.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const logoutButton = document.getElementById('logout');
+    const logoutButton = document.getElementById('logout');
 
-  logoutButton.addEventListener('click', () => {
-    window.location.href = 'login.html';
-  });
+    logoutButton.addEventListener('click', () => {
+        window.location.href = 'login.html';
+    });
 
-  const menuButton = document.querySelector('.menu');
-  const nav = document.getElementById('sidebar');
-  const backdrop = document.getElementById('backdrop');
+    const menuButton = document.querySelector('.menu');
+    const nav = document.getElementById('sidebar');
+    const backdrop = document.getElementById('backdrop');
 
-  function toggleMenu() {
-    nav.classList.toggle('open');
-    backdrop.classList.toggle('visible');
-  }
+    function toggleMenu() {
+        nav.classList.toggle('open');
+        backdrop.classList.toggle('visible');
+    }
 
-  menuButton.addEventListener('click', toggleMenu);
-  backdrop.addEventListener('click', toggleMenu);
+    menuButton.addEventListener('click', toggleMenu);
+    backdrop.addEventListener('click', toggleMenu);
+
+    const descripcion = document.getElementById("descripcion");
+
+    function autoAjustar() {
+        descripcion.style.height = "auto";
+        const bordes = descripcion.offsetHeight - descripcion.clientHeight;
+        descripcion.style.height = descripcion.scrollHeight + bordes + "px";
+    }
+
+    descripcion.addEventListener("input", autoAjustar);
+
 });
